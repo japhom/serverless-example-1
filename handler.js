c89a744a5be0d75c1c60407ecc7ca526d0ce763b
Guillermo Villalobos <guillermo@pixan.io>
2018-10-14 00:32:29 -0500

    Added dynamo reading capabilities

--- handler.js
+++ handler.js
@@ -1,19 +1,42 @@
 'use strict';
+var AWS = require("aws-sdk");
+var dynamoDBConfiguration = {
+   "accessKeyId": "AKIAJRL3HRX3LH3GTPLQ",
+   "secretAccessKey": "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",
+   "region": "us-east-1"
+};
 
-module.exports.hello = async (event, context) => {
+AWS.config.update(dynamoDBConfiguration);
+var dynClient = new AWS.DynamoDB.DocumentClient();
 
-  var restaurants = [{
-    name: 'Vips'
-  }, {
-    name: 'Toks'
-  }];
+module.exports.hello = (event, context, callback) => {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      restaurants: restaurants,
-    }),
+  var params = {
+    TableName: 'restaurants'
   };
+  dynClient.scan(params, function(err, data) {
+    if(err){
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Error fetching restaurant list',
+      });
+      return;
+		}
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        restaurants: data.Items
+      }),
+    };
+
+    callback(null, response);
+
+  });
+
+
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
